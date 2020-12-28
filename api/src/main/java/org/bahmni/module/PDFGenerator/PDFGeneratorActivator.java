309bb398439d711c8c348dcc5ad61ad113b7c4d9
Mahesh-I <mahesh.ippili@thoughtworks.com>
2020-12-28 17:27:12 +0530

    PDFGenerator basic module created

--- api/src/main/java/org/bahmni/module/PDFGenerator/PDFGeneratorActivator.java
+++ api/src/main/java/org/bahmni/module/PDFGenerator/PDFGeneratorActivator.java
@@ -11,31 +11,22 @@
  *
  * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
  */
-package org.openmrs.module.PDFGenerator;
+package org.bahmni.module.PDFGenerator;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.openmrs.module.BaseModuleActivator;
 
-/**
- * This class contains the logic that is run every time this module is either started or shutdown
- */
 public class PDFGeneratorActivator extends BaseModuleActivator {
 	
 	private Log log = LogFactory.getLog(this.getClass());
-	
-	/**
-	 * @see org.openmrs.module.Activator#startup()
-	 */
+
 	public void startup() {
-		log.info("Starting Basic Module");
+		log.info("Starting PDF Generator");
 	}
-	
-	/**
-	 * @see org.openmrs.module.Activator#shutdown()
-	 */
+
 	public void shutdown() {
-		log.info("Shutting down Basic Module");
+		log.info("Shutting down PDF Generator");
 	}
 	
 }
